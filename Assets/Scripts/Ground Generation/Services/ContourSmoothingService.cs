c5e5bd497a29b4a1464bf13c1a8d2a13b81d2214
Iain Stanford <i_stanford@hotmail.com>
2017-11-13 22:28:52 +0000

    ContourSmoothingService setup and does contour smoothing and vertex removal

--- Assets/Scripts/Ground Generation/Services/ContourSmoothingService.cs
+++ Assets/Scripts/Ground Generation/Services/ContourSmoothingService.cs
@@ -2,15 +2,80 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ContourSmoothingService : MonoBehaviour {
+public class ContourSmoothingService : IContourSmoothingService
+{
+    public int[] m_smoothWeights = new int[] { 1, 2, 1 };
+    public int m_numSmoothingPasses = 5;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void SmoothContours(ref List<GroundChunk> chunks)
+    {
+        foreach (var chunk in chunks)
+        {
+            chunk.Poly = null;
+            //Smooth Contours
+            for(int i=0; i<m_numSmoothingPasses; i++)
+                SmoothContour(chunk.Edge);
+
+            foreach (var hole in chunk.Holes)
+                for(int i=0; i<m_numSmoothingPasses; i++)
+                    SmoothContour(hole);
+        }
+    }
+
+    private void SmoothContour(VertexSequence vs)
+    {
+        VertexSequence smooth = new VertexSequence();
+        for (int i = 0; i < vs.Count; i++)
+        {
+            float x = 0;
+            float y = 0;
+            int w = 0;
+            for (int j = -m_smoothWeights.Length / 2; j <= m_smoothWeights.Length / 2; j++)
+            {
+                x += vs[i + j].X * m_smoothWeights[j + (m_smoothWeights.Length / 2)];
+                y += vs[i + j].Y * m_smoothWeights[j + (m_smoothWeights.Length / 2)];
+                w += m_smoothWeights[j + (m_smoothWeights.Length / 2)];
+            }
+            x /= w;
+            y /= w;
+            smooth.Add(new Point() { X = x, Y = y });
+        }
+
+        vs.CopyFrom(smooth);
+    }
+
+    public void RemoveVertices(ref List<GroundChunk> chunks)
+    {
+        foreach (var chunk in chunks)
+        {
+            chunk.Poly = null;
+            //Smooth Contours
+            RemoveVertices(chunk.Edge);
+            foreach (var hole in chunk.Holes)
+                RemoveVertices(hole);
+        }
+    }
+
+    private void RemoveVertices(VertexSequence vs)
+    {
+        int count = 0;
+        for (int i = 0; i < vs.Count; i++)
+        {
+            Point a = vs[i - 1];
+            Point b = vs[i];
+            Point c = vs[i + 1];
+
+            //Remove b if its the same X or same Y as a and c
+            bool abX = UnityEngine.Mathf.Approximately(a.X, b.X);
+            bool bcX = UnityEngine.Mathf.Approximately(b.X, c.X);
+            bool abY = UnityEngine.Mathf.Approximately(a.Y, b.Y);
+            bool bcY = UnityEngine.Mathf.Approximately(b.Y, c.Y);
+            if ((abX && bcX) || (abY && bcY))
+            {
+                count++;
+                vs.Remove(b);
+                i--;
+            }
+        }
+    }
 }
--- Assets/Scripts/Ground Generation/Services/Interfaces/IContourSmoothingService.cs
+++ Assets/Scripts/Ground Generation/Services/Interfaces/IContourSmoothingService.cs
@@ -2,15 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IContourSmoothingService : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public interface IContourSmoothingService
+{
+    void SmoothContours(ref List<GroundChunk> chunks);
+    void RemoveVertices(ref List<GroundChunk> chunks);
 }
