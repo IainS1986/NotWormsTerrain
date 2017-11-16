66c8d961a6adf8535e889006a72fe918d45a11f0
Iain Stanford <i_stanford@hotmail.com>
2017-11-16 21:39:47 +0000

    Starting work on adding the Lips to the ground chunks. Gonna be tricky but trying to build the mesh as efficently as possible.

--- Assets/Scripts/Ground Generation/Services/MeshService.cs
+++ Assets/Scripts/Ground Generation/Services/MeshService.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class MeshService : IMeshService
 {
+    private static Mesh sStraightMesh;
+    private static int[] sStraightVertexPairings;
+    private static int[] sStraightLeftHandVerts;
+    private static int[] sStraightRightHandVerts;
+
     private static Material[] Materials = new Material[2];
+    private static Material[] Materials_Lips = new Material[2];
 
     public MeshService()
     {
@@ -14,8 +20,56 @@ public class MeshService : IMeshService
             (Material)Resources.Load("Materials/Earth"),
             (Material)Resources.Load("Materials/Stone")
         };
+
+        Materials_Lips = new Material[]
+        {
+            null,
+            (Material)Resources.Load("Materials/Earth_Lip"),
+            (Material)Resources.Load("Materials/Stone_Lip")
+        };
+
+        //Build the straight mesh we use as a template for lips, placing each template at each vertexsequence segment
+        BuildStraightMesh();
     }
 
+    private static void BuildStraightMesh()
+    {
+        Mesh straight = new Mesh();
+
+        Vector3[] verts = new Vector3[6];
+        int[] tris = new int[4 * 3];
+        Vector2[] uvs = new Vector2[6];
+        Vector3[] norms = new Vector3[6];
+
+        verts[0] = new Vector3(-0.5f, -0.5f, 0f);
+        verts[1] = new Vector3(-0.5f, 0, 0f);
+        verts[2] = new Vector3(-0.5f, 0, 2.5f);
+
+        verts[3] = new Vector3(0.5f, -0.5f, 0f);
+        verts[4] = new Vector3(0.5f, 0, 0f);
+        verts[5] = new Vector3(0.5f, 0, 2.5f);
+
+        int t = 0;
+        tris[t++] = 0; tris[t++] = 1; tris[t++] = 4;
+        tris[t++] = 4; tris[t++] = 3; tris[t++] = 0;
+        tris[t++] = 1; tris[t++] = 2; tris[t++] = 5;
+        tris[t++] = 5; tris[t++] = 4; tris[t++] = 1;
+
+        sStraightVertexPairings = new int[] { 3, 4, 5, 0, 1, 2 };
+        sStraightLeftHandVerts = new int[] { 0, 1, 2 };
+        sStraightRightHandVerts = new int[] { 3, 4, 5 };
+
+        straight.vertices = verts;
+        straight.uv = uvs;
+        straight.triangles = tris;
+        straight.normals = norms;
+
+        straight.RecalculateNormals();
+
+        sStraightMesh = straight;
+    }
+
+    #region Main Mesh
     public void BuildMesh(Ground ground)
     {
         BuildMesh(ground.Chunks);
@@ -44,7 +98,7 @@ public class MeshService : IMeshService
             MeshRenderer mr = chunk.GameObject.AddComponent<MeshRenderer>();
 
             mf.mesh = chunk.Mesh;
-        
+
             mr.material = Materials[chunk.GroundType];
         }
 
@@ -75,4 +129,49 @@ public class MeshService : IMeshService
         //mesh.Optimize();
         chunk.Mesh.RecalculateNormals();
     }
+    #endregion
+
+    #region Lips
+    public void BuildLips(Ground ground)
+    {
+        BuildLips(ground.Chunks);
+    }
+
+    public void BuildLips(Dictionary<int, GroundChunk> chunks)
+    {
+        foreach(var chunk in chunks)
+        {
+            BuildLips(chunk.Value);
+        }
+    }
+
+    private void BuildLips(GroundChunk chunk)
+    {
+        BuildLipForContour(chunk.Edge, Materials_Lips[chunk.GroundType]);
+
+        if(chunk.Holes!= null)
+        {
+            foreach(var hole in chunk.Holes)
+            {
+                BuildLipForContour(hole, Materials_Lips[chunk.GroundType]);
+            }
+        }
+    }
+
+    private void BuildLipForContour(VertexSequence contour, Material material)
+    {
+        Mesh lip = new Mesh();
+
+        //Each point the contour has 3 verts connected to the next point on the contour. The verts are in an L-Shape
+        //It sits over the edge of the main contour mesh
+        int totalVerts = contour.Count * 3;
+
+        Vector3[] verts = new Vector3[totalVerts];
+        Vector3[] norms = new Vector3[totalVerts];
+        Vector2[] uvs = new Vector2[totalVerts];
+
+        //The L-Shape has 4 tris per segment. 2 forming the rectangle on the face, 2 forming the rectangle on the "floor/roof/depth"
+        int[] tris = new int[contour.Count * 4];
+    }
+    #endregion
 }
---
+++ Assets/Scripts/Utility/MeshExtensionMethods.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MeshExtensionMethods
+{
+
+	public static Mesh Copy(this Mesh m)
+    {
+        Mesh copy = new Mesh();
+
+        Vector3[] verts_orig = m.vertices;
+        Vector3[] norms_orig = m.normals;
+        Vector2[] uvs_orig = m.uv;
+        int[] tris_orig = m.triangles;
+
+        Vector3[] verts = new Vector3[verts_orig.Length];
+        Vector3[] norms = new Vector3[norms_orig.Length];
+        Vector2[] uvs = new Vector2[uvs_orig.Length];
+        int[] tris = new int[tris_orig.Length];
+
+        for (int i = 0; i < verts.Length; i++) verts[i] = verts_orig[i];
+        for (int i = 0; i < norms.Length; i++) norms[i] = norms_orig[i];
+        for (int i = 0; i < uvs.Length; i++) uvs[i] = uvs_orig[i];
+        for (int i = 0; i < tris.Length; i++) tris[i] = tris_orig[i];
+
+        copy.vertices = verts;
+        copy.uv = uvs;
+        copy.normals = norms;
+        copy.triangles = tris;
+
+        return copy;
+    }
+}
