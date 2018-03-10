0e8964614948b0faaade219a2b703b86f4be4769
Iain Stanford <i_stanford@hotmail.com>
2018-03-10 21:31:39 +0000

    Simple contour culler, not great but works for now

--- Assets/Scripts/Ground Generation/Services/Concrete/ContourOptimiserService.cs
+++ Assets/Scripts/Ground Generation/Services/Concrete/ContourOptimiserService.cs
@@ -13,26 +13,40 @@ namespace Terrain.Services.Concrete
 
         public void RemoveSmallContours(Ground ground, Dictionary<int, GroundChunk> chunks)
         {
+            List<int> chunksToRemove = new List<int>();
             foreach (var chunk in chunks)
             {
                 if(ContourIsTooSmall(chunk.Value.Edge))
                 {
                     //Whole chunk needs removing...
+                    chunksToRemove.Add(chunk.Key);
+                    continue;
                 }
 
+                List<VertexSequence> holesToRemove = new List<VertexSequence>();
                 foreach (var hole in chunk.Value.Holes)
                 {
                     if(ContourIsTooSmall(hole))
                     {
                         //Fill in the hole
+                        holesToRemove.Add(hole);
                     }
                 }
+
+                foreach (var hole in holesToRemove)
+                    chunk.Value.Holes.Remove(hole);
             }
+
+            foreach (int id in chunksToRemove)
+                chunks.Remove(id);
         }
 
+        // TODO would be nice for this to actually review poly surface area....but for now lets keep
+        // it super simple and just cull any contours under a certain vertex length...as we can assume they
+        // will be too small.
         private bool ContourIsTooSmall(VertexSequence seq)
         {
-            return false;
+            return (seq == null) ? true : seq.Count < 10;
         }
     }
 }
